d56df49bd9640b8c8412d737f0f1b2bdd46ec48c
axylos <robertdraketalley@gmail.com>
2020-03-29 16:22:54 +0000

    tmp

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const { PeerServer } = require('peer'); 
-PeerServer({port: 9001, path: '/karaoke-api'})
+const express = require('express');
+const logger = require('morgan');
+const { ExpressPeerServer } = require('peer'); 
+
+const app = express();
+app.use(logger('dev'));
+const server = app.listen(9001);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/'
+});
+
+peerServer.on('connection', (client) => {
+  console.log('got a conn');
+  console.log(client);
+  client.send('open conn');
+});
+
+app.use('/karaoke-api', peerServer);
--- client/src/svc.js
+++ client/src/svc.js
@@ -1,14 +1,20 @@
 import Peer from 'peerjs'
 const config = {
   host: 'draketalley.com',
-  path: '/karaoke-api'
+  path: '/karaoke-api',
+  secure: true,
+  'iceServers': [{ 'urls': ['stun:stun.l.google.com:19302']  }],
+  debug: 3
 }
 let peer;
 let handle;
 let localStream;
+const first = "FIRST";
+const second = "SECOND";
 export const mountVid = () => {
+
   const me = document.querySelector('#me');
-  navigator.getUserMedia({video: true, audio: true}, (stream) => {
+  navigator.getUserMedia({video: true}, (stream) => {
     me.srcObject = stream;
     me.muted = true;
     me.play();
@@ -17,17 +23,22 @@ export const mountVid = () => {
   return null;
 }
 export const makeFoo = () => {
-  peer = new Peer('first', config);
+  peer = new Peer(first, config);
+  peer.on('open', () => {
+    console.log('open');
+  }); 
+  peer.on('error', err => console.log('first err: ', err));
+  peer.on('close', () => console.log('peer destroyed'));
+  peer.on('disconnected', () => console.log('disconnected'));
 
   peer.on('connection', (conn) => {
     handle = conn;
+    console.log('connection');
     conn.on('data', (data) => {
       console.log(data);
     });
+    conn.on('open', () => console.log('opening'));
 
-    conn.on('open', () => {
-      conn.send('hello');
-    }); 
 
   });
   peer.on('call', (call) => {
@@ -35,7 +46,7 @@ export const makeFoo = () => {
 
     call.on('stream', (stream) => {
 
-const partner = document.querySelector('#you');
+      const partner = document.querySelector('#you');
       partner.srcObject = stream;
       //partner.muted = true;
       partner.play();
@@ -44,10 +55,10 @@ const partner = document.querySelector('#you');
 };
 
 export const call = () => {
-  peer = new Peer('second', config);
-  const x = peer.call('first', localStream);
+  peer = new Peer(second, config);
+  const x = peer.call(first, localStream);
   x.on('stream', (otherStream) => {
-const partner = document.querySelector('#you');
+    const partner = document.querySelector('#you');
     partner.srcObject = otherStream;
     partner.play();
   });
@@ -56,16 +67,30 @@ export const send = (e) => {
   e.preventDefault();
   const form = new FormData(e.target);
   const txt = form.get('query');
+  console.log(txt);
   handle.send(txt);
 }
 export const connectFoo = () => {
-  peer = new Peer('second', config);
-  const conn = peer.connect('first');
-  handle = conn;
-  conn.on('open', () => {
-    conn.send('hi');
+  peer = new Peer(second, config);
+  peer.on('open', () => {
+    console.log('open');
   });
-};
+
+  const conn = peer.connect(first);
+  handle = conn;
+  window.handle = handle;
+  console.log(conn);
+  conn.on('error', err => console.log('error: ', err));
+  conn.on('open', () => console.log('second conn opened'));
+
+
+  conn.on('data', (data) => console.log(data));
+  conn.on('close', () => console.log('closed'));
+
+  peer.on('close', () => console.log('closed peer'));
+  peer.on('error', err => console.log(err));
+  peer.on('disconnected', () => console.log('disconnected'));
+}; 
 
 
 
